483b694a10645a7183a8d8b6bf96cf06673b8d09
Thomas LE CLERE <tleclere@jahia.com>
2024-06-13 17:48:20 +0200

    Move status code to bad request (400)

--- src/main/java/org/jahia/community/modules/CrossSitesFilter.java
+++ src/main/java/org/jahia/community/modules/CrossSitesFilter.java
@@ -2,7 +2,6 @@ package org.jahia.community.modules;
 
 import org.apache.commons.lang.StringUtils;
 import org.jahia.bin.filters.AbstractServletFilter;
-import org.jahia.bin.filters.ServletFilter;
 import org.jahia.services.seo.urlrewrite.ServerNameToSiteMapper;
 import org.osgi.service.component.annotations.Component;
 
@@ -16,11 +15,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component(service = AbstractServletFilter.class)
-public class CrossSitesFilter extends ServletFilter {
+public class CrossSitesFilter extends AbstractServletFilter {
     private static final String URI_PART_SITES = "/sites/";
 
     public CrossSitesFilter() {
-        setUrlPatterns(new String[]{".*" + URI_PART_SITES + ".*"});
+        setMatchAllUrls(true);
         setOrder(0.5f);
     }
 
@@ -33,10 +32,13 @@ public class CrossSitesFilter extends ServletFilter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest hsRequest = (HttpServletRequest) request;
         final HttpServletResponse hsResponse = (HttpServletResponse) response;
-        final String siteKeyFromPath = StringUtils.substringBetween(hsRequest.getRequestURI(), URI_PART_SITES, "/");
-        final String siteKeyFromServerName = ServerNameToSiteMapper.getSiteKeyByServerName(hsRequest);
-        if (siteKeyFromPath != null && !siteKeyFromServerName.isEmpty() && !siteKeyFromPath.equals(siteKeyFromServerName)) {
-            hsRequest.setAttribute("prevent_cross_script", "true");
+        final String requestURI = hsRequest.getRequestURI();
+        if (requestURI.contains(URI_PART_SITES)) {
+            final String siteKeyFromPath = StringUtils.substringBetween(requestURI, URI_PART_SITES, "/");
+            final String siteKeyFromServerName = ServerNameToSiteMapper.getSiteKeyByServerName(hsRequest);
+            if (siteKeyFromPath != null && !siteKeyFromServerName.isEmpty() && !siteKeyFromPath.equals(siteKeyFromServerName)) {
+                hsRequest.setAttribute("prevent_cross_script", "true");
+            }
         }
         chain.doFilter(hsRequest, hsResponse);
     }
