d5b06c88a85cc32f1c5855960c0ff3281d2fada8
Thomas LE CLERE <tleclere@jahia.com>
2024-06-13 17:09:05 +0200

    Review filter configuration

--- src/main/java/org/jahia/community/modules/CrossSitesFilter.java
+++ src/main/java/org/jahia/community/modules/CrossSitesFilter.java
@@ -1,7 +1,11 @@
 package org.jahia.community.modules;
 
-import java.io.IOException;
-import javax.servlet.Filter;
+import org.apache.commons.lang.StringUtils;
+import org.jahia.bin.filters.AbstractServletFilter;
+import org.jahia.bin.filters.ServletFilter;
+import org.jahia.services.seo.urlrewrite.ServerNameToSiteMapper;
+import org.osgi.service.component.annotations.Component;
+
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
@@ -9,44 +13,36 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang.StringUtils;
-import org.jahia.bin.filters.AbstractServletFilter;
-import org.jahia.services.seo.urlrewrite.ServerNameToSiteMapper;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ServiceScope;
+import java.io.IOException;
 
-@Component(service = AbstractServletFilter.class, scope = ServiceScope.BUNDLE)
-public class CrossSitesFilter extends AbstractServletFilter implements Filter {
-
+@Component(service = AbstractServletFilter.class)
+public class CrossSitesFilter extends ServletFilter {
     private static final String URI_PART_SITES = "/sites/";
 
-    @Activate
-    public void activate() {
-        setMatchAllUrls(true);
+    public CrossSitesFilter() {
+        setUrlPatterns(new String[]{".*" + URI_PART_SITES + ".*"});
         setOrder(0.5f);
     }
 
     @Override
-    public void init(FilterConfig cfg) throws ServletException {
+    public void init(FilterConfig cfg) {
+        // Do nothing
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletRequest hsRequest = (HttpServletRequest) request;
         final HttpServletResponse hsResponse = (HttpServletResponse) response;
-        final String uri = hsRequest.getRequestURI();
-        if (uri.contains(URI_PART_SITES)) {
-            final String siteKeyFromPath = StringUtils.substringBetween(uri, URI_PART_SITES, "/");
-            final String siteKeyFromServerName = ServerNameToSiteMapper.getSiteKeyByServerName(hsRequest);
-            if (siteKeyFromPath != null && !siteKeyFromServerName.isEmpty() && !siteKeyFromPath.equals(siteKeyFromServerName)) {
-                hsRequest.setAttribute("prevent_cross_script", "true");
-            }
+        final String siteKeyFromPath = StringUtils.substringBetween(hsRequest.getRequestURI(), URI_PART_SITES, "/");
+        final String siteKeyFromServerName = ServerNameToSiteMapper.getSiteKeyByServerName(hsRequest);
+        if (siteKeyFromPath != null && !siteKeyFromServerName.isEmpty() && !siteKeyFromPath.equals(siteKeyFromServerName)) {
+            hsRequest.setAttribute("prevent_cross_script", "true");
         }
         chain.doFilter(hsRequest, hsResponse);
     }
 
     @Override
     public void destroy() {
+        // Do nothing
     }
 }
